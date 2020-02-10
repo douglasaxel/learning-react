ba9eb20174f462e4903a0583283438b705d24684
Douglas Kjellin <douglasaxelkjellin@gmail.com>
2020-02-10 01:10:10 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Message from './components/Message'
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      {/* <Message name="Douglas" /> */}
+    </div>
+  );
+}
+
+export default App;
